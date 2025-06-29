3543ba5948f5fabea3cd6867b5368c54247040ff
Sahil Gupta <guptasahil2175@gmail.com>
2025-06-29 13:47:34 +0530

    Creating login form and rendering to user also checking authenticate of user that exist or not

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
 
 router.get('/signup', (req, res) => {
     res.render('./users/signup');
@@ -23,5 +24,17 @@ router.post('/signup', wrapAsync(
         }
     }
 ));
+
+router.get('/login', (req, res) => {
+    res.render('./users/login');
+});
+
+router.post('/login', passport.authenticate("local", {failureRedirect: '/login', failureFlash: true}), 
+    async(req, res) => {
+        let { username } = req.body;
+        req.flash("success", `Welcome to Wanderlust, ${username}! You have successfully logged in.`);
+        res.redirect('/listings');
+    }
+);
 
 module.exports = router;
