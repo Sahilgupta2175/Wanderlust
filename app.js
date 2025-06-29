85e2f708753604ba063e1fb05db4d366f9cfdc5c
Sahil Gupta <guptasahil2175@gmail.com>
2025-06-29 12:03:34 +0530

    Configuring passport strategy

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ const listings = require('./routes/listing');
 const reviews = require('./routes/review');
 const session = require('express-session');
 const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('./models/user');
 
 const mongo_url = 'mongodb://localhost:27017/wanderlust';
 
@@ -46,6 +49,14 @@ app.get('/', (req, res) => {
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => { 
     res.locals.success = req.flash('success');
