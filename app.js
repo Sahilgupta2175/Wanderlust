824532b202d4b2e0c5eaa63fb76b1bd68f93d7a6
Sahil Gupta <guptasahil2175@gmail.com>
2025-07-10 19:07:10 +0530

    Conveting our hardcode secret key into variable which will be  access from .env file

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.static(path.join(__dirname, "/public")));
 const store = MongoStore.create({
     mongoUrl: dbURL,
     crypto: {
-        secret: "mysupersecretcode"
+        secret: process.env.SECRET_KEY
     },
     touchAfter: 24 * 60 * 60 // 24 hours
 });
@@ -52,7 +52,7 @@ store.on('error', (err) => {
 
 const sessionOptions = {
     store: store,
-    secret: 'mysupersecretcode',
+    secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: true,
     cookie: {
