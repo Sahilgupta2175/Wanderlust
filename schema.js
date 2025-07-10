fb7654a29f717642050feeb916f3f6a6de51117f
Sahil Gupta <guptasahil2175@gmail.com>
2025-07-10 12:57:42 +0530

    Adding category field validation check field is given or not

--- schema.js
+++ schema.js
@@ -4,6 +4,10 @@ module.exports.listingSchema = Joi.object({
     listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
+        category: Joi.alternatives().try(
+            Joi.array().items(Joi.string()),
+            Joi.string()
+        ),
         price: Joi.number().required().min(0),
         location: Joi.string().required(),
         country: Joi.string().required(),
